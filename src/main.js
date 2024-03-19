d8ebba44c49fd9171f0f06fe9eb98aa07412265c
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-03-19 16:51:56 +0800

    still fixing the bot command output responses (quick commit)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-const { rejects } = require('assert');
-const { connect } = require('http2');
-const { resolve } = require('path');
-const { exit } = require('process');
-const { stream } = require('undici-types');
-
 const fs = require('fs');
 const util = require('util');
 const writeFile = util.promisify(fs.writeFile);
@@ -11,12 +5,12 @@ const readFile = util.promisify(fs.readFile);
 const unlink = util.promisify(fs.unlink);
 
 const ssh2 = require('ssh2');
-
+require('dotenv').config();
 
 let sshSessions = [];
 
 // create a new SSH session.
-async function createShellSession(host, port, username, password) {
+async function createShellSession(uid, host, port, username, password) {
     return new Promise((resolve, reject) => {
         const client = new ssh2.Client();
 
@@ -45,51 +39,61 @@ async function createShellSession(host, port, username, password) {
     uid: the user id of the person who is trying to connect.
     credentials: the credentials to use to connect.
 */
-async function connectShellSession(uid, credentials) {
+async function connectShellSession(uid, credentials, attempts = 0) {
     return new Promise((resolve, reject) => {
         const session = sshSessions.find(session => session.uid === uid);
-        if (!session || !session.client) {
-            createShellSession(credentials.host, credentials.port, credentials.username, credentials.password).then((client) => {
-                sshSessions.push({ uid, client });
-                return resolve(client);
+        // use uid to find if the user has an active SSH session, and just reconnect to it.
+        if (session && session.client) return resolve(session.client);
 
+        // else if the user has no active SSH session, create a new one.
+        createShellSession(uid, credentials.host, credentials.port, credentials.username, credentials.password).then((client) => {
+            sshSessions.push({ uid, client });
+            return resolve(client);
+
+        // if the input credentials are wrong, then try to load the credentials from the file and use it to connect.
+        }).catch((err) => {
+            if (attempts == 5) return reject('Error: Failed to connect to the SSH server. Please update your credentials using the `sshd` command.');
+
+            loadShellCredentials(uid).then((credentials) => {
+                connectShellSession(uid, credentials, attempts + 1).then((client) => {
+                    return resolve(client);
+                }).catch((err) => {
+                    return reject(err);
+                });
+                
+            // else, error.
             }).catch((err) => {
                 return reject(err);
             });
-        } else {
-            return resolve(session.client);
-        }
+        });
     });
 }
 
-
-async function executeShellCommand(uid, command) {
+// idk what to name the SSH, so its 'client' for now.
+async function executeShellCommand(client, command) {
     return new Promise(async (resolve, reject) => {
-        let credentials;
+        const stripAnsi = await import('strip-ansi');
 
-        try {
-            credentials = await loadShellCredentials(uid);
-        } catch (err) {
-            return reject(err);
-        }
-        
-        try {
-            const client = await connectShellSession(uid, credentials);
+        client.shell((err, stream) => {
+            if (err) return reject(err);
             
-            client.shell((err, stream) => {
-                if (err) return reject(err);
+            let output = '';
 
-                stream.on('close', () => client.end());
-                stream.on('data', (data) => console.log(data.toString()) );
+            stream.on('close', () => client.end());
+            stream.on('data', (data) => {
 
-                // execute the command here.
-                stream.write(`${command}\n`);
+                output += stripAnsi.default(data.toString());
+                writeFile('output.txt', output);
+                
+                // take only the command output using REGEX.
 
-                return resolve('Command executed successfully.');
-            })
-        } catch (err) {
-            return reject(err);
-        }
+            });
+
+            // execute the command here.
+            stream.write(`${command}\n`);
+            
+            return resolve(output);
+        });
     });
 }
 
@@ -129,3 +133,82 @@ function deleteShellCredentials(uid) {
         })
         .catch(err => Promise.reject(err));
 }
+
+
+
+/* ==================================================================================================== */
+// The following code is for the Discord bot.
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once("ready", () => {
+    console.log("client is online.");
+});
+
+client.on("messageCreate", async (message) => {
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+    const userId = interaction.user.id;
+
+    if (commandName === 'sshd') {
+        const host = interaction.options.getString('host');
+        const port = interaction.options.getInteger('port');
+        const username = interaction.options.getString('username');
+        const password = interaction.options.getString('password');
+
+        connectShellSession(userId, { host, port, username, password }).then((client) => {
+            interaction.reply('SSH session started successfully.');
+        }).catch((err) => {
+            interaction.reply(err);
+        });
+    }
+
+    else if (commandName === 'exit') {
+        exitShellSession(userId).then((result) => {
+            interaction.reply(result);
+        }).catch((err) => {
+            interaction.reply(err);
+        });
+    }
+
+    else if (commandName === 'purge') {
+        deleteShellCredentials(userId).then((result) => {
+            interaction.reply(result);
+        }).catch((err) => {
+            interaction.reply(err);
+        });
+    }
+
+    else if (commandName === 'ssh') {
+        // should i find by the array or load from the file?
+        
+        const command = interaction.options.getString('command');
+        const credendentials = loadShellCredentials(userId);
+        
+        connectShellSession(userId, credendentials).then((client) => {
+            executeShellCommand(client, command).then((result) => {
+                interaction.reply(result);
+            }).catch((err) => {
+                interaction.reply(err);
+            });
+        }).catch((err) => {
+            interaction.reply(err);
+        });
+    }
+});
+
+client.login(process.env.TOKEN);
---
+++ src/register_commands.js
@@ -0,0 +1,71 @@
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+
+const commands = [
+    {
+        name: 'sshd',
+        description: 'Start an SSH session. If no credentials are provided, the bot will attempt to use saved credentials.',
+        options: [
+            {
+                name: 'host',
+                description: 'The host to connect to.',
+                type: 3,
+                required: false
+                
+            },
+            {
+                name: 'port',
+                description: 'The port to connect to.',
+                type: 4,
+                required: false
+            },
+            {
+                name: 'username',
+                description: 'The username to use.',
+                type: 3,
+                required: false
+            },
+            {
+                name: 'password',
+                description: 'The password to use.',
+                type: 3,
+                required: false
+            }
+        ]
+    },
+    {
+        name: 'exit',
+        description: 'Exit the current SSH session.',
+        options: []
+    },
+    {
+        name: 'purge',
+        description: 'Delete your saved SSH credentials.',
+    },
+    {
+        name: 'ssh',
+        description: 'Execute a command in the current SSH session.',
+        options: [
+            {
+                name: 'command',
+                description: 'The command to execute.',
+                type: 3,
+                required: true
+            }
+        ]
+    }
+]
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+      
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+      
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();

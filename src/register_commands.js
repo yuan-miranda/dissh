01f08b1c83a4b71d31b015beae986717188faac4
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-03-22 14:55:56 +0800

    updated the id name for the bot

--- src/register_commands.js
+++ src/register_commands.js
@@ -62,7 +62,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
     try {
         console.log('Started refreshing application (/) commands.');
       
-        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+        await rest.put(Routes.applicationCommands(process.env.BOT_ID), { body: commands });
       
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {

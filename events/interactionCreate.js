1bfb22e1c9e8793b2e9636595a6777a5afc4a5c0
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-05-26 14:10:29 +0800

    working "View in Text" button

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,23 +1,38 @@
 import { Events, MessageFlags } from "discord.js";
+import { readFileSync } from "fs";
+import { __dirname } from "../index.js";
 
 export const name = Events.InteractionCreate;
 export async function execute(interaction) {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
+    if (interaction.isChatInputCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+            }
+        }
+    } else if (interaction.isButton()) {
+        if (interaction.customId !== "text_view") return;
+        try {
+            await interaction.deferReply({ ephemeral: true });
+            let content = readFileSync(`${__dirname}/data/messages/${interaction.message.id}.txt`, 'utf8');
+
+            if (content.length >= 2000) content = content.substring(content.length - 1900);
+            await interaction.editReply({ content: `\`\`\`bash\n${content}\n\`\`\``});
+        } catch (error) {
+            console.error(error);
+            await interaction.editReply({ content: 'There was an error while retrieving the message content.', flags: MessageFlags.Ephemeral });
         }
     }
 }
